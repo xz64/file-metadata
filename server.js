e73bd55f9b40f0571e333184e80c9ace5626607b
S K <xz64@users.noreply.github.com>
2016-03-27 21:52:08 -0400

    add server file size processing code

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 var express = require('express');
 var multer = require('multer');
 
+var UPLOAD_LIMIT_MB = 5;
+
+var storage = multer.memoryStorage();
+var upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: UPLOAD_LIMIT_MB * 1024 * 1024
+  }
+}).single('file');
+
 var port = +process.env.PORT || 8080;
 var app = express();
+
+app.post('/api/upload', function(req, res) {
+  upload(req, res, function(err)  {
+    if(err) {
+      return res.json({error: 'Exceeded file limit size of ' + UPLOAD_LIMIT_MB
+        + ' MB.'});
+    }
+    res.json({size: req.file.size});
+  });
+});
 
 app.listen(port);
